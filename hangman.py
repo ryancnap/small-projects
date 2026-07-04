3601b5cbb3d3e21c7edfdf964a36fa21c051e492
Ryan Camaratta <ryancnap@gmail.com>
2026-07-04 00:16:08 -0400

    Refactor logic into functions

--- hangman.py
+++ hangman.py
@@ -1,36 +1,52 @@
 import random
 
-attempts = 0
-number_appearances = 0
-incorrect_guesses = 0
-incorrect_guess_max = 6
-blank_character = '_'
-
 wordlist = ['stallman', 'kernighan', 'ritchie', 'torvalds', 'djikstra']
 word = random.choice(wordlist)
 word_mask = ['_' for char in word]
+guess_cache = []
+
+def show_score():
+    print()
+    print('_'.join(word_mask))
+    print(f'<> Incorrect guesses: {incorrect_guesses} / {incorrect_guess_max}')
+    print(f'<> Already guessed: {[i for i in guess_cache if i not in word]}')
+    print()
+
+def process_guess(guess, incorrect_guesses):
+    if guess in guess_cache:
+        print(f'* You already tried {guess.upper()}')
+
+    elif guess not in word:
+        incorrect_guesses += 1
+        guess_cache.append(guess)
+        print(f' ! {guess.upper()} does not appear in the word ! ')
+
+    elif guess in word:
+        guess_cache.append(guess)
+        print(f' ! {word.count(guess)}x {guess.upper()} found in the word !')
+
+    return incorrect_guesses
 
 def check_won():
-    return blank_character not in word_mask
+    return '_' not in word_mask
 
+def update_word_mask(guess):
+     for index, letter in enumerate(word):
+        if letter == guess:
+            word_mask[index] = guess
+
+incorrect_guesses = 0
+incorrect_guess_max = 6
 solved = False
 while not solved and incorrect_guesses < incorrect_guess_max:
-    attempts += 1
-    print(word_mask)
+    show_score()
     guess = input('\nSelect a letter > ').lower()
+    incorrect_guesses = process_guess(guess, incorrect_guesses)
 
-    if guess not in word:
-        incorrect_guesses += 1
-        print(f'{guess} does not appear in the word.')
+    update_word_mask(guess)
+    solved = check_won()
 
-    if guess in word:
-        print(f' * {word.count(guess)}x {guess.upper()} found in the word!')
 
-        for index, letter in enumerate(word):
-            if letter == guess:
-                word_mask[index] = guess
-
-        solved = check_won()
 
         
     
