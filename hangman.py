8b658ebdcdd134908298fef80631876e16ebc303
Ryan Camaratta <ryancnap@gmail.com>
2026-07-04 00:44:36 -0400

    Refactor to explicitly pass state

--- hangman.py
+++ hangman.py
@@ -1,18 +1,18 @@
 import random
 
-wordlist = ['stallman', 'kernighan', 'ritchie', 'torvalds', 'djikstra']
-word = random.choice(wordlist)
+WORD_LIST = ['stallman', 'kernighan', 'ritchie', 'torvalds', 'djikstra']
+word = random.choice(WORD_LIST)
 word_mask = ['_' for char in word]
 guess_cache = []
 
 def show_score():
     print()
     print('_'.join(word_mask))
-    print(f'<> Incorrect guesses: {incorrect_guesses} / {incorrect_guess_max}')
+    print(f'<> Incorrect guesses: {incorrect_guesses} / {INCORRECT_GUESS_MAX}')
     print(f'<> Already guessed: {[i for i in guess_cache if i not in word]}')
     print()
 
-def process_guess(guess, incorrect_guesses):
+def process_guess(guess, incorrect_guesses, guess_cache, word):
     if guess in guess_cache:
         print(f'* You already tried {guess.upper()}')
 
@@ -27,24 +27,24 @@ def process_guess(guess, incorrect_guesses):
 
     return incorrect_guesses
 
-def check_won():
+def check_won(word_mask):
     return '_' not in word_mask
 
-def update_word_mask(guess):
+def update_word_mask(guess, word, word_mask):
      for index, letter in enumerate(word):
         if letter == guess:
             word_mask[index] = guess
 
 incorrect_guesses = 0
-incorrect_guess_max = 6
+INCORRECT_GUESS_MAX = 6
 solved = False
-while not solved and incorrect_guesses < incorrect_guess_max:
+while not solved and incorrect_guesses < INCORRECT_GUESS_MAX:
     show_score()
     guess = input('\nSelect a letter > ').lower()
-    incorrect_guesses = process_guess(guess, incorrect_guesses)
+    incorrect_guesses = process_guess(guess, incorrect_guesses, guess_cache, word)
 
-    update_word_mask(guess)
-    solved = check_won()
+    update_word_mask(guess, word, word_mask)
+    solved = check_won(word_mask)
 
 
 
